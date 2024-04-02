e2872503502e83fb6a663c524b650f6799f8828e
UmeshGayashan <umeshgayashan099@gmail.com>
2024-04-02 20:13:06 +0530

    first commit

---
+++ src/UDPClient.java
@@ -0,0 +1,25 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class UDPClient {
+    public static void main(String[] args) throws Exception {
+        //Special Socket for UDP ==> DatagramSocket
+        DatagramSocket ds = new DatagramSocket();
+        int i = 8;
+        byte[] b = (i+"").getBytes();
+        InetAddress ia = InetAddress.getLocalHost();
+        DatagramPacket dp = new DatagramPacket(b,b.length,ia,9999);
+        ds.send(dp);
+
+        byte[] b1 = new byte[1024];
+        DatagramPacket dp1 = new DatagramPacket(b1,b1.length);
+        ds.receive(dp1);
+
+        String str = new String(dp1.getData());
+        System.out.println("Result is "+str);
+
+
+    }
+}
