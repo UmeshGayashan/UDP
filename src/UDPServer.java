1d06fbb45f5507972e040c9051e7e4dffeb2254f
UmeshGayashan <umeshgayashan099@gmail.com>
2024-04-02 21:23:14 +0530

    Object names are Changed

--- src/UDPServer.java
+++ src/UDPServer.java
@@ -5,18 +5,18 @@ import java.net.SocketException;
 
 public class UDPServer {
     public static void main(String a[]) throws Exception {
-        DatagramSocket ds = new DatagramSocket(9999);
+        DatagramSocket dsS = new DatagramSocket(9999);
         byte[] b1 = new byte[1024];
 
-        DatagramPacket dp = new DatagramPacket(b1,b1.length);
-        ds.receive(dp);
-        String str = new String(dp.getData(),0, dp.getLength());
+        DatagramPacket dpS = new DatagramPacket(b1,b1.length);
+        dsS.receive(dpS);
+        String str = new String(dpS.getData(),0, dpS.getLength());
         int num = Integer.parseInt(str.trim());
         int result = num*num;
         byte[] b2 = (result + "").getBytes();
         InetAddress ia = InetAddress.getLocalHost();
-        DatagramPacket dp1 = new DatagramPacket(b2,b2.length,ia,dp.getPort());
-        ds.send(dp1);
+        DatagramPacket dp1 = new DatagramPacket(b2,b2.length,ia,dpS.getPort());
+        dsS.send(dp1);
      }
 
 }
